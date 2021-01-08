cb42a043016c244473829161d31e648f45022247
farleysm8909 <farleysm8909@uwec.edu>
2021-01-07 18:20:30 -0600

    Studio 2: Counting Characters

---
+++ src/org/launchcode/java/studios/countingcharacters/CountChars.java
@@ -0,0 +1,26 @@
+package org.launchcode.java.studios.countingcharacters;
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountChars {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+        String str = input.nextLine();
+        char[] charsInStr = str.toCharArray();
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (char character : charsInStr) {
+            if (charCount.containsKey(character)) {
+                charCount.put(character, charCount.get(character) + 1); //.get() method not in textbook, but returns value for given key
+            } else {
+                charCount.put(character, 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> pair : charCount.entrySet()) {
+            System.out.println(pair.getKey() + ": " + pair.getValue());
+        }
+    }
+}
